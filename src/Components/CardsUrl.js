309bbd4b48e9408138fe6fb5d66a25f89c1c6382
Edwin3002 <vargas11742@gmail.com>
2022-07-16 16:45:31 -0500

    final design

--- src/Components/CardsUrl.js
+++ src/Components/CardsUrl.js
@@ -13,7 +13,8 @@ export const CardsUrl = ({ data }) => {
     }
     return (
         <div  className='flex flex-col lg:flex-row rounded-lg bg-white p-4 mt-8'>
-            <p className='w-full lg:w-3/6 py-4 textUrlLong border-b-2 lg:border-b-0 border-[hsl(225, 33%, 95%)]'>{data.urlL}</p>
+            <p className='w-full lg:w-3/6 py-4 textUrlLong '>{data.urlL}</p>
+            <hr className='lg:hidden'/>
             <p className='textUrlShort w-2/6 py-4 lg:mx-4 text-left lg:text-right'>{data.urlS}</p>
             <span className='flex mx-auto w-full lg:w-1/6'>
                 <button className={` ${copy ? 'btn': 'btn2'} text-center rounded-lg w-full  mx-auto`} onClick={() => copyUrl(data.urlS)}>{copy? 'Copy': 'Copied!'}</button>
--- src/Components/Navb.js
+++ src/Components/Navb.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Navb = () => {
+    const [navB, setNavB] = useState(false)
     return (
-        <nav className='flex justify-around my-8'>
-            <div className='flex w-full xl:hidden '>
+        <nav className='flex justify-around pb-8'>
+            <div className='flex flex-col w-full xl:hidden fixed z-20 bg-white py-4'>
                 <ul className='flex w-10/12 mx-auto justify-between items-center'>
                     <li>
                         <img className='mx-auto' alt='Shortly' src='https://res.cloudinary.com/edwin3002/image/upload/v1657844426/seedem/logo_jgfdfh.svg' />
                     </li>
                     <li>
-                        <img className='mx-auto' alt='Shortly' src='https://res.cloudinary.com/edwin3002/image/upload/v1657905135/seedem/ham_nurjku.svg' />
+                        <img onClick={()=>setNavB(!navB)} className='mx-auto' alt='Shortly' src='https://res.cloudinary.com/edwin3002/image/upload/v1657905135/seedem/ham_nurjku.svg' />
                     </li>
                 </ul>
+                {navB ? <div className='flex justify-center w-full xl:flex mx-auto navbMobile mt-4'>
+                    <ul className='flex flex-col text-center absolute  mx-auto w-11/12 py-4  font-bold navBarMobile rounded-xl'>
+                        <li >
+                            <a onClick={()=>setNavB(!navB)} href='#'>Features</a>
+                        </li>
+                        <li>
+                            <a onClick={()=>setNavB(!navB)} href='#'>Pricing</a>
+                        </li>
+                        <li className='w-4/5 my-0 pb-8 border-b-2 lg:border-b-0 border-[hsl(225, 33%, 95%)]'>
+                            <a onClick={()=>setNavB(!navB)} href='#'>Resourses</a>
+                        </li>
+                        <li>
+                            <a onClick={()=>setNavB(!navB)} href='#'>Login</a>
+                        </li>
+                        <li className='w-4/5 text-center'>
+                            <button onClick={()=>setNavB(!navB)} href='#' className='btn  w-full'>Sign Up</button>
+                        </li>
+                    </ul>
+                </div> :
+                    null}
             </div>
-            <div className='hidden w-1/3 xl:flex '>
+            <div className='hidden w-1/3 xl:flex xl:py-8'>
                 <ul className='flex w-full justify-between items-center text-navBar font-bold'>
                     <li>
                         <img className='mx-auto' alt='hamburger' src='https://res.cloudinary.com/edwin3002/image/upload/v1657844426/seedem/logo_jgfdfh.svg' />
@@ -35,7 +56,7 @@ export const Navb = () => {
                         <a href='#'>Login</a>
                     </li>
                     <li>
-                        <button  href='#' className='btn'>Sign Up</button>
+                        <button href='#' className='btn'>Sign Up</button>
                     </li>
                 </ul>
             </div>
