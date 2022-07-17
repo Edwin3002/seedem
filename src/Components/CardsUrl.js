874f2d64dd0fea02b4d6d20ae0b7210909fc2557
Edwin Vargas Ayala <88119497+Edwin3002@users.noreply.github.com>
2022-07-16 21:07:19 -0500

    Update CardsUrl.js

--- src/Components/CardsUrl.js
+++ src/Components/CardsUrl.js
@@ -13,7 +13,7 @@ export const CardsUrl = ({ data }) => {
     }
     return (
         <div  className='flex flex-col lg:flex-row rounded-lg bg-white p-4 mt-8'>
-            <p className='w-full lg:w-3/6 pb-4 textUrlLong '>{data.urlL}</p>
+            <p className='w-full lg:w-3/6 lg:pt-4 pb-4 textUrlLong '>{data.urlL}</p>
             <hr className='lg:hidden'/>
             <p className='textUrlShort w-2/6 py-4 lg:mx-4 text-left lg:text-right'>{data.urlS}</p>
             <span className='flex mx-auto w-full lg:w-1/6'>
